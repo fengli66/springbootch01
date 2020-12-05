da818f530d1a32692c1f665b02c1614574adc5eb
fengli66 <276084876@qq.com>
2020-12-05 16:23:53 +0800

    第一个springboot项目创建

---
+++ src/main/java/com/lifeng/Springbootch01Application.java
@@ -0,0 +1,13 @@
+package com.lifeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootch01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootch01Application.class, args);
+	}
+
+}
